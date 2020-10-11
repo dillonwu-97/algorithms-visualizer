26c06b268bcd3838df912d83f1852762c83ff26a
Dillon Wu <dillonwu999@gmail.com>
2020-10-11 04:50:53 -0400

    app changes debug

--- src/App.js
+++ src/App.js
@@ -10,28 +10,28 @@ import { HashRouter, Route, Link, Switch } from "react-router-dom";
 function App() {
 	return (
 		<div>
-			<MyNavbar/>
+			{/* <MyNavbar/>
 			<HashRouter>
 				<Switch>
 					<Route exact path='/pathfinder' component = {Pathfinder}/>
 					<Route exact path='/sudoku' component={Sudoku}/>
 				</Switch>
-			</HashRouter>
+			</HashRouter> */}
 		</div>
 	);
 }
-function Panel({ title, subtitle, doSomething }) {
-	return (
-	<div className="panel">
-	  <h2 className="panel__title">{title}</h2>
-	  <img src="some/url"/>
-	   <div panel__content>
-		<h3 className="panel__subtitle">{subtitle}</h3>
-	   </div>
-		<button class="action-button" onClick={doSomething}>Click me</button>
-	 </div>
-   );
-  } 
+// function Panel({ title, subtitle, doSomething }) {
+// 	return (
+// 	<div className="panel">
+// 	  <h2 className="panel__title">{title}</h2>
+// 	  <img src="some/url"/>
+// 	   <div panel__content>
+// 		<h3 className="panel__subtitle">{subtitle}</h3>
+// 	   </div>
+// 		<button class="action-button" onClick={doSomething}>Click me</button>
+// 	 </div>
+//    );
+//   } 
   
 function Pathfinder() {
 	return (
