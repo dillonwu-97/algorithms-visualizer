02ef9016f9021cc8647b6ca6b303e049d1472db5
Dillon Wu <dillonwu999@gmail.com>
2020-10-19 14:32:09 -0400

    made the pathfinding visualizer button look better

---
+++ build/precache-manifest.dde1175a1b30d1c5e9e109cae025beb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477d2f17e21f2279412247ca9142bf8b",
+    "url": "/algorithms-visualizer/index.html"
+  },
+  {
+    "revision": "9498360fba849f04fb43",
+    "url": "/algorithms-visualizer/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "01743154bfead0f3d489",
+    "url": "/algorithms-visualizer/static/css/main.c40579f3.chunk.css"
+  },
+  {
+    "revision": "9498360fba849f04fb43",
+    "url": "/algorithms-visualizer/static/js/2.8a513df7.chunk.js"
+  },
+  {
+    "revision": "756c1a6046d68fe705005e2773270c5f",
+    "url": "/algorithms-visualizer/static/js/2.8a513df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01743154bfead0f3d489",
+    "url": "/algorithms-visualizer/static/js/main.3daa663a.chunk.js"
+  },
+  {
+    "revision": "0480c75e1d87d270f057",
+    "url": "/algorithms-visualizer/static/js/runtime-main.d06e165b.js"
+  },
+  {
+    "revision": "ab0bcc97638a30818887efc55ec376dc",
+    "url": "/algorithms-visualizer/static/media/pathfinder_1.ab0bcc97.gif"
+  }
+]);
