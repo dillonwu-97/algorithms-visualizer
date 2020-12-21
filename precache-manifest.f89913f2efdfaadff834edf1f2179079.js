aab4eda2623fbc59dabdc546e833ee036b5061f9
Dillon Wu <dillonwu999@gmail.com>
2020-12-21 17:07:06 -0500

    Updates

---
+++ precache-manifest.f89913f2efdfaadff834edf1f2179079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daec3c62f5d37c9c044583cd87879f76",
+    "url": "/algorithms-visualizer/index.html"
+  },
+  {
+    "revision": "0588e2c63c0f79760da7",
+    "url": "/algorithms-visualizer/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7cfeb3c17cf3c4589f85",
+    "url": "/algorithms-visualizer/static/css/main.7121e2fc.chunk.css"
+  },
+  {
+    "revision": "0588e2c63c0f79760da7",
+    "url": "/algorithms-visualizer/static/js/2.f0ae195d.chunk.js"
+  },
+  {
+    "revision": "756c1a6046d68fe705005e2773270c5f",
+    "url": "/algorithms-visualizer/static/js/2.f0ae195d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfeb3c17cf3c4589f85",
+    "url": "/algorithms-visualizer/static/js/main.6605ec02.chunk.js"
+  },
+  {
+    "revision": "0480c75e1d87d270f057",
+    "url": "/algorithms-visualizer/static/js/runtime-main.d06e165b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/algorithms-visualizer/precache-manifest.49b9e55ce7677261a77a061ac30eb8b2.js"
+  "/algorithms-visualizer/precache-manifest.f89913f2efdfaadff834edf1f2179079.js"
 );
 
 self.addEventListener('message', (event) => {
