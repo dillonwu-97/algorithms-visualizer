8691b2c31756603feaefb6df948eaf9bce664dad
Dillon Wu <dillonwu999@gmail.com>
2020-12-30 12:33:02 -0500

    playing around with d3

--- src/trees/sandbox.js
+++ src/trees/sandbox.js
@@ -1,6 +1,7 @@
 import *  as d3 from "d3"
 import React, { Component } from 'react'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useSpring, animated } from 'react-spring'
 
 // Useful resource for learning d3 and react https://wattenberger.com/blog/react-and-d3
 
@@ -23,20 +24,92 @@ const Svg = () => {
     )
 }
 
-const Circle = () => {
-    const ref = useRef() // stores reference to rendered svg element
 
-    useEffect(() => {
-        // d3.select turns ref into d3 selection object
-        const svgElement = d3.select(ref.current)
-        svgElement.append("circle")
-            .attr("cx", 150)
-            .attr("cy", 70)
-            .attr("r", 50)
-    }, [])
+
+// useinterval taken from: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+const circleCount = 10
+
+const generateDataset = () => {
+    return Array(circleCount).fill(0).map(i=>[Math.round(Math.random() * 100), Math.round(Math.random() * 100)])
+}
+
+const generateVisible = () => {
+    return Array(circleCount).fill(0).map(i=>Math.round(Math.random()))
+}
+
+const AllCircles = () => {
+    // const [circleDataset, createDataset] = useState(generateDataset())
+    // this one is better because the above is called every time state is rerendered
+    // whereas the one below only renders at the start
+    // not entirely sure why
+    const [circleDataset, createDataset] = useState(() => generateDataset())
+    const [visibleCircles, createVisible] = useState(() => generateVisible())
+
+    function setVisible() {
+        createVisible(generateVisible())
+    }
 
     return (
-        <svg ref = {ref} />
+        <div>
+            {console.log(visibleCircles)}
+            <svg>
+                {circleDataset.map((val, index) => (
+                    <Circle
+                        x = {val[0]}
+                        y = {val[1]}
+                        visible = {visibleCircles[index]}
+                    />
+                ))}
+            </svg>
+
+            <button onClick = {() => setVisible()}> Click me </button>
+        </div>
+    )
+
+}
+
+
+const Circle = ({ x, y, visible}) => {
+
+    // use spring for these styles
+    const style = useSpring({
+        config: {
+            duration: 1200,
+            mass: 1,
+            tension: 170,
+            friction:24
+        },
+        r: visible? "10": "0",
+        opacity: visible? "1": "0",
+        fill: visible? "blue": "red"
+    })
+    
+    return (
+        <animated.circle {...style}
+            cx = {x}
+            cy = {y}
+            // r = {visible == 1? "10": "0"}
+        />
     )
 }
 
@@ -48,12 +121,10 @@ export default class Trees extends Component {
         }
     }
 
-    
-
     render() {
         return (
             <div>
-                <Circle />
+                <AllCircles />
             </div>
         )
     }
--- src/trees/test.js
+++ src/trees/test.js
@@ -1,3 +1,90 @@
 const d3 = require('d3')
 
-const Circle 
+
+
+
+// const Circle = () => {
+//     const ref = useRef() // stores reference to rendered svg element
+
+//     useEffect(() => {
+//         // d3.select turns ref into d3 selection object
+//         const svgElement = d3.select(ref.current)
+//         svgElement.append("circle")
+//             .attr("cx", 150)
+//             .attr("cy", 70)
+//             .attr("r", 50)
+//     }, [])
+
+//     return (
+//         <svg ref = {ref} />
+//     )
+// }
+
+// const Circle = () => {
+//     return (
+//         <svg>
+//             <circle 
+//                 cx = "150"
+//                 cy = "70"
+//                 r = "50"
+//             />
+//         </svg>
+//     )
+// }
+
+// this does not work / is buggy
+// const Circle = () => {
+//     const [dataset, setdataset] = useState(generateDataset())
+//     const ref = useRef()
+
+//     useEffect(() => {
+//         // select all elements that are of the element circle i think?
+//         const svgElement = d3.select(ref.current)
+//         svgElement.selectAll("circle")
+//         .data(dataset)
+//         .join("circle")
+//         .attr("cx", d=>dataset[0])
+//         .attr("cy", d=>dataset[1])
+//         .attr("r", 3)
+//     }, [dataset])
+
+//     // fires off every 2 seconds
+//     useInterval(() => {
+//         const d = generateDataset()
+//         setdataset(d)
+//     }, 2000)
+
+//     return (
+//         <svg viewBox ="0 0 100 50" ref = {ref}/>
+
+//     )
+
+// }
+
+
+// used to show circles at random locations using d3
+// const Circle = () => {
+//     const [data, setdata] = useState(generateDataset())
+//     useInterval(() => {
+//         const newdata = generateDataset()
+//         setdata(newdata)
+//     }, 2000)
+//     console.log(data)
+//     return (
+
+//         // x = 0 y = 0 width = 100% height = 50%
+//         <svg viewBox= "0 0 100 50" >
+//             {data.map(i => (
+//                 // console.log("i is ", i)
+//                 <circle 
+//                     cx = {i[0]}
+//                     cy = {i[1]}
+//                     r = "3"
+//                     fill = "tomato"
+//                 />
+//             ))}
+//         </svg>
+
+//     )
+
+// }
